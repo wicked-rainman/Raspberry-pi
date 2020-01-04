bda0d824057f7375fd2f73afe5da3786bed3f672
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2020-01-04 12:54:32 +0000

    Update bright.c

--- Micro Dot Phat/bright.c
+++ Micro Dot Phat/bright.c
@@ -1,22 +1,14 @@
 //-------------------------------------------------------------------------
-// The 0x0d lighting effect register in the IS31FL3730 chip defines the
-// amout of current delivered to each display row. Values are:
-//
-// 0000 (0) = 40ma
-// 0001 (1) = 45ma
-// 0010 (2) = 50ma
-// 0011 (3) = 55ma
-// 0100 (4) = 60ma
-// 0101 (5) = 65ma
-// 0110 (6) = 70ma
-// 0111 (7) = 75ma
-//
-// Therefore values between 0 and 7 can be used
+// bright - Change the light intensity for the display
+// 0x19 is the pulse width modulation register in the IS31FL3730 chip.
+// There are 3 chips on board, 0x61, 0x62 and 0x63
+// Valid values for PWM are not obvious, but through trial and error, 0-7
+// seem to work. Anything after 8 seems to make no difference.
 //-------------------------------------------------------------------------
 void bright(int fd, int val) {
         uint8_t buf[2],k,chip_addr=0x61;
         if(fd<0) return;
-        buf[0]=0x19;    //Brightness
+        buf[0]=0x19;    //Brightness PWM reg
         buf[1]=val;     //Brightness value
         for(k=0;k<3;k++) {
                 ioctl(fd, I2C_SLAVE,chip_addr+k);
