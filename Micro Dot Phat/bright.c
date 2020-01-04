92eb383027e553bc0b2dea6296e6a18455c4d3d1
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2020-01-04 12:57:21 +0000

    Update bright.c

--- Micro Dot Phat/bright.c
+++ Micro Dot Phat/bright.c
@@ -4,14 +4,16 @@
 // There are 3 chips on board, 0x61, 0x62 and 0x63
 // Valid values for PWM are not obvious, but through trial and error, 0-7
 // seem to work. Anything after 8 seems to make no difference.
+//
+// WR
 //-------------------------------------------------------------------------
 void bright(int fd, int val) {
         uint8_t buf[2],k,chip_addr=0x61;
-        if(fd<0) return;
-        buf[0]=0x19;    //Brightness PWM reg
-        buf[1]=val;     //Brightness value
-        for(k=0;k<3;k++) {
-                ioctl(fd, I2C_SLAVE,chip_addr+k);
-                write(fd,buf,2);
+        if(fd<0) return;                                //Device hasn't been opened
+        buf[0]=0x19;                                    //Brightness PWM reg
+        buf[1]=val;                                     //Brightness value
+        for(k=0;k<3;k++) {                              //Deal with the 3 chips
+                ioctl(fd, I2C_SLAVE,chip_addr+k);       //Select the chip
+                write(fd,buf,2);                        //Write the register
         }
 }
