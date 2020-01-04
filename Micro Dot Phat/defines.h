f06aed0c9f88ba87fb532e9c668495c9ade678a4
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2020-01-04 13:38:54 +0000

    Delete defines.h

--- Micro Dot Phat/defines.h
+++
@@ -1,8 +0,0 @@
-#define I2C_DEV "/dev/i2c-1"
-#define DEFAULT_MODE 0x18
-#define CMD_MODE    0x00
-#define CMD_UPDATE  0x0C
-#define CMD_BRIGHT  0x19
-#define CMD_MATRIX1 0x01
-#define CMD_MATRIX2 0x0E
-#define CMD_RESET   0xFF
