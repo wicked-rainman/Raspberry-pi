a1c637d1c0c25ae70693bfd7564d4f6dbcff4513
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2020-01-04 13:14:26 +0000

    Update reset.c

--- Micro Dot Phat/reset.c
+++ Micro Dot Phat/reset.c
@@ -1,3 +1,16 @@
+//-----------------------------------------------------------------
+// Reset the display
+// Reset the 3 IS31FL3730 chips then set them back in default mode
+// 0xff is the chip Reset register
+// 0x00 is the chip Configuration register:
+// Default mode (0x18) = 0b00011000,sets display mode to use
+// two matrix displays and sets intensity current to be controlled 
+// by the Lighting Effect Register.
+// Chips have addresses 0x61, 9x62 and 0x63 on the bus. 
+//
+// WR
+//-----------------------------------------------------------------
+
 void reset(int fd) {
         uint8_t buf1[2]={0xff,0x00}; //Reset
         uint8_t buf2[2]={0x00,0x18}; //Default mode
