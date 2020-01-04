9cb7ac881bbcb1ea0339592c24495e56ca0b7d89
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2020-01-04 09:55:27 +0000

    Create defines.h

---
+++ Micro Dot Phat/defines.h
@@ -0,0 +1,10 @@
+#define I2C_DEV "/dev/i2c-1"
+#define MDH_ADR 0x61
+#define DEFAULT_MODE 0x18
+#define CMD_MODE    0x00
+#define CMD_UPDATE  0x0C
+#define CMD_OPTION  0x0D
+#define CMD_BRIGHT  0x19
+#define CMD_MATRIX1 0x01
+#define CMD_MATRIX2 0x0E
+#define CMD_RESET   0xFF
